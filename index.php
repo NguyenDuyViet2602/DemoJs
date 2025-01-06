ed852156d6df1b2971170aad7fc4fb670da5781a
NguyenDuyViet2602 <nguyenduyviet369@gmail.com>
2025-01-06 17:20:59 +0700

    update project

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
-<?php
-echo "Xin chao Nguyen Duy Hiep"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type="text/css">
+        h1,h2{
+            margin-left: 100px
+        }
+        .buttom {
+            background-color: orange;
+            font-size: 30px;
+            border-radius: 100px;
+            border:none;
+            width: 80%;
+            height: 80px ;
+            margin-left: 100px;
+            color: white;
+
+        }
+        .text {
+            font-size: 15px;
+            border: none;
+            border-bottom: 2px solid #fff0ff;
+            height: 20px;
+            width: 20%;
+            margin-left: 100px;
+            margin-bottom: 50px
+        }
+    </style>
+</head>
+<body>
+    <form action="">
+    <div><h1>Giải Phương Trình Bậc 2</h1></div>
+    <br>
+    <div>
+        <input type="text" class = "text" placeholder = "Nhập a" >
+        <input type="text" class = "text" placeholder = "Nhập b">
+        <input type="text" class = "text" placeholder = "Nhập c">
+    </div>
+    <br>
+    <input type="submit" value="Giải Phương Trình" class="buttom">
+    <div><h2>Kết quả</h2></div>
+    </form>
+
+</body>
+</html>
